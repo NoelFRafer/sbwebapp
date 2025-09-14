ee1586e415f00d33258151565704879cf77a4d27
NoelFRafer <noelfrafer@gmail.com>
2025-09-14 22:46:24 +0800

    Updated ResolutionsPage.tsx

--- src/components/ResolutionsPage.tsx
+++ src/components/ResolutionsPage.tsx
@@ -214,7 +214,8 @@ export function ResolutionsPage() {
                       </span>
                     )}
                     {resolution.with_ordinance && (
-                      <span className="inline-flex items-center px-4 py-0 text-xs font-medium bg-gray-800 text-blue-100 rounded-full border border-blue-500">
+                      <span className="inline-flex items-center px-4 py-0 text-xs font-medium bg-gray-3
+                        00 text-blue-100 rounded-full border border-blue-500">
                         With Ordinance
                       </span>
                     )}
