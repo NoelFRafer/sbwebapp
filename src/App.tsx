70a6663c994e2f46340fc4cd88558beb2bbc5933
NoelFRafer <noelfrafer@gmail.com>
2025-09-16 20:56:55 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
     totalNewsItems,
     itemsPerPage: newsItemsPerPage,
     setCurrentPage: setNewsCurrentPage
-  } = useNews(debouncedNewsSearchTerm, 5, newsFilterFeatured, newsFilterPriority);
+  } = useNews(debouncedNewsSearchTerm, 4, newsFilterFeatured, newsFilterPriority);
 
   const clearNewsSearch = () => {
     setNewsSearchTerm('');
